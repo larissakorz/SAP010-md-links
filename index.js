c100808c2f204507434a8c533326f500c518f1fe
Larissa Korz <lariskorz@gmail.com>
2023-07-03 21:55:12 -0300

    instalação e testes para pegar links

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-module.exports = () => {
-  // ...
-};
+// LER ARQUIVO
+const fs = require('fs')
+
+fs.readFile('./README.md', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  // console.log(data)
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
+  const links = data.match(regex);
+  console.log(links);
+})
+
+
+// // EXTENSAO DE UM ARQUIVO
+// const path = require('path');
+
+// const caminhoArquivo = './src/exemplo.txt';
+// const extensao = path.extname(caminhoArquivo);
+// console.log(extensao);
+
+// // Obter o conteúdo de um diretório
+// fs.readdir('./src/', (err, files) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(files);
+// });
+
+// // Definir rotas
+// require('path').dirname('./src/exemplo.txt')
---
+++ src/outroExemplo.md
@@ -0,0 +1,1 @@
+testando mais um arquivo
